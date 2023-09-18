5c8075ef6b6406869e908ed05a4a433d721a88a8
tomlinconnor <144960978+tomlinconnor@users.noreply.github.com>
2023-09-18 10:42:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -44,7 +44,7 @@ export const managers = [
  //     "philosophy": "Your fantasy team's philosophy",
  //     "tradingScale": 10, // 1 - 10
  //     "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
+ //   },
     {
       "roster": 2,// ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "477973406533283840",
